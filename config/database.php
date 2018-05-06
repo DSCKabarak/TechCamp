6b1867be89f3ca79843daa77f8af50e72aae3e05
Sam Bell <me@sam-bell.com>
2018-05-06 12:41:58 +0100

    Option to set DB port

--- config/database.php
+++ config/database.php
@@ -61,6 +61,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST'),
+            'port'	=> env('DB_PORT'),
             'database'  => env('DB_DATABASE'),
             'username'  => env('DB_USERNAME'),
             'password'  => env('DB_PASSWORD'),
