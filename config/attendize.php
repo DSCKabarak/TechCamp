8cb7c92efba279413a3715d60ef8ea338f452fd8
Jeremy Quinton <jeremyquinton@gmail.com>
2018-07-05 17:22:29 +0200

    added default config option
    1) added configuration option to enable dummy payment gateways

--- config/attendize.php
+++ config/attendize.php
@@ -7,8 +7,8 @@ return [
     'ticket_status_sold_out'        => 1,
     'ticket_status_after_sale_date' => 2,//
     'enable_test_payments'          => env('ENABLE_TEST_PAYMENTS', false),
-
     'enable_dummy_payment_gateway'  => false,
+    'payment_gateway_dummy'    => 0,
     'payment_gateway_stripe'   => 1,
     'payment_gateway_paypal'   => 2,
     'payment_gateway_coinbase' => 3,
@@ -18,6 +18,7 @@ return [
         'expiryMonth' => '6',
         'expiryYear' => '2030',
         'cvv' => '123'
+    ],
     'outgoing_email_noreply' => env('MAIL_FROM_ADDRESS'),
     'outgoing_email'         => env('MAIL_FROM_ADDRESS'),
     'outgoing_email_name'    => env('MAIL_FROM_NAME'),
