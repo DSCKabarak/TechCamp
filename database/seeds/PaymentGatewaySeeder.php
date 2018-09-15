e8d69469bea56643d97ee8f4bd8ff68938761c88
Jeremy Quinton <jeremyquinton@gmail.com>
2018-09-15 21:17:51 +0200

    remove migs and coinbase as default gateways

--- database/seeds/PaymentGatewaySeeder.php
+++ database/seeds/PaymentGatewaySeeder.php
@@ -28,23 +28,7 @@ class PaymentGatewaySeeder extends Seeder
                 'is_on_site' => 0,
                 'can_refund' => 0
 
-            ],
-            [
-                'id' => 3,
-                'name' => 'Coinbase',
-                'provider_name' => 'Coinbase',
-                'provider_url' => 'https://coinbase.com',
-                'is_on_site' => 0,
-                'can_refund' => 0,
-            ],
-            [
-                'id' => 4,
-                'name' => 'Migs_ThreeParty',
-                'provider_name' => 'MasterCard Internet Gateway Service',
-                'provider_url' => 'https://www.mastercard.com/gateway/payment-processing/online-credit-card-and-debit-card-payment-processing.html',
-                'is_on_site' => 0,
-                'can_refund' => 0,
-            ],
+            ]
         ];
 
         DB::table('payment_gateways')->insert($payment_gateways);
