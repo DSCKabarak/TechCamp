09335d7aeeadf42d52df525f13ad613c8b172713
Jeremy Quinton <jeremyquinton@gmail.com>
2018-09-16 20:36:17 +0200

    added image reference

--- resources/views/Public/ViewEvent/Partials/EventCreateOrderSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventCreateOrderSection.blade.php
@@ -228,6 +228,7 @@
             </div>
         </div>
     </div>
+    <img src="https://cdn.attendize.com/lg.png" />
 </section>
 @if(session()->get('message'))
     <script>showMessage('{{session()->get('message')}}');</script>
