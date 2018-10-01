a2da46ce5ad5e79db92b380cb66e31b727b232b2
johanricher <johan.richer@gmail.com>
2018-10-01 16:58:08 +0200

    setup to start translation

---
+++ resources/views/fr/Emails/OrderNotification.blade.php
@@ -0,0 +1,130 @@
+@extends('en.Emails.Layouts.Master')
+
+@section('message_content')
+Hello,<br><br>
+
+You have received a new order for the event <b>{{$order->event->title}}</b>.<br><br>
+
+@if(!$order->is_payment_received)
+    <b>Please note: This order still requires payment.</b>
+    <br><br>
+@endif
+
+
+Order Summary:
+<br><br>
+Order Reference: <b>{{$order->order_reference}}</b><br>
+Order Name: <b>{{$order->full_name}}</b><br>
+Order Date: <b>{{$order->created_at->toDayDateTimeString()}}</b><br>
+Order Email: <b>{{$order->email}}</b><br>
+
+
+<h3>Order Items</h3>
+<div style="padding:10px; background: #F9F9F9; border: 1px solid #f1f1f1;">
+
+    <table style="width:100%; margin:10px;">
+        <tr>
+            <th>
+                Ticket
+            </th>
+            <th>
+                Quantity
+            </th>
+            <th>
+                Price
+            </th>
+            <th>
+                Booking Fee
+            </th>
+            <th>
+                Total
+            </th>
+        </tr>
+        @foreach($order->orderItems as $order_item)
+        <tr>
+            <td>
+                {{$order_item->title}}
+            </td>
+            <td>
+                {{$order_item->quantity}}
+            </td>
+            <td>
+                @if((int)ceil($order_item->unit_price) == 0)
+                FREE
+                @else
+                {{money($order_item->unit_price, $order->event->currency)}}
+                @endif
+
+            </td>
+            <td>
+                @if((int)ceil($order_item->unit_price) == 0)
+                -
+                @else
+                {{money($order_item->unit_booking_fee, $order->event->currency)}}
+                @endif
+
+            </td>
+            <td>
+                @if((int)ceil($order_item->unit_price) == 0)
+                FREE
+                @else
+                {{money(($order_item->unit_price + $order_item->unit_booking_fee) * ($order_item->quantity), $order->event->currency)}}
+                @endif
+
+            </td>
+        </tr>
+        @endforeach
+        <tr>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+                <b>Sub Total</b>
+            </td>
+            <td colspan="2">
+                {{$orderService->getOrderTotalWithBookingFee(true)}}
+            </td>
+        </tr>
+        @if($order->event->organiser->charge_tax == 1)
+        <tr>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+                <b>{{$order->event->organiser->tax_name}}</b>
+            </td>
+            <td colspan="2">
+                {{$orderService->getTaxAmount(true)}}
+            </td>
+        </tr>
+        @endif
+        <tr>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+                <b>Total</b>
+            </td>
+            <td colspan="2">
+                {{$orderService->getGrandTotal(true)}}
+            </td>
+        </tr>
+    </table>
+
+
+    <br><br>
+    You can manage this order at: {{route('showEventOrders', ['event_id' => $order->event->id, 'q'=>$order->order_reference])}}
+    <br><br>
+</div>
+<br><br>
+Thank you
+@stop
---
+++ resources/views/fr/Mailers/TicketMailer/SendOrderTickets.blade.php
@@ -0,0 +1,125 @@
+@extends('en.Emails.Layouts.Master')
+
+@section('message_content')
+Hello,<br><br>
+
+Your order for the event <b>{{$order->event->title}}</b> was successful.<br><br>
+
+Your tickets are attached to this email. You can also view you order details and download your tickets at: {{route('showOrderDetails', ['order_reference' => $order->order_reference])}}
+
+@if(!$order->is_payment_received)
+<br><br>
+<b>Please note: This order still requires payment. Instructions on how to make payment can be found on your order page: {{route('showOrderDetails', ['order_reference' => $order->order_reference])}}</b>
+<br><br>
+@endif
+<h3>Order Details</h3>
+Order Reference: <b>{{$order->order_reference}}</b><br>
+Order Name: <b>{{$order->full_name}}</b><br>
+Order Date: <b>{{$order->created_at->toDayDateTimeString()}}</b><br>
+Order Email: <b>{{$order->email}}</b><br>
+<a href="{!! route('downloadCalendarIcs', ['event_id' => $order->event->id]) !!}">Add To Calendar</a>
+<h3>Order Items</h3>
+<div style="padding:10px; background: #F9F9F9; border: 1px solid #f1f1f1;">
+    <table style="width:100%; margin:10px;">
+        <tr>
+            <td>
+                <b>Ticket</b>
+            </td>
+            <td>
+                <b>Qty.</b>
+            </td>
+            <td>
+                <b>Price</b>
+            </td>
+            <td>
+                <b>Fee</b>
+            </td>
+            <td>
+                <b>Total</b>
+            </td>
+        </tr>
+        @foreach($order->orderItems as $order_item)
+                                <tr>
+                                    <td>
+                                        {{$order_item->title}}
+                                    </td>
+                                    <td>
+                                        {{$order_item->quantity}}
+                                    </td>
+                                    <td>
+                                        @if((int)ceil($order_item->unit_price) == 0)
+                                        FREE
+                                        @else
+                                       {{money($order_item->unit_price, $order->event->currency)}}
+                                        @endif
+
+                                    </td>
+                                    <td>
+                                        @if((int)ceil($order_item->unit_price) == 0)
+                                        -
+                                        @else
+                                        {{money($order_item->unit_booking_fee, $order->event->currency)}}
+                                        @endif
+
+                                    </td>
+                                    <td>
+                                        @if((int)ceil($order_item->unit_price) == 0)
+                                        FREE
+                                        @else
+                                        {{money(($order_item->unit_price + $order_item->unit_booking_fee) * ($order_item->quantity), $order->event->currency)}}
+                                        @endif
+
+                                    </td>
+                                </tr>
+                                @endforeach
+        <tr>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+                <b>Sub Total</b>
+            </td>
+            <td colspan="2">
+                {{$orderService->getOrderTotalWithBookingFee(true)}}
+            </td>
+        </tr>
+        @if($order->event->organiser->charge_tax == 1)
+        <tr>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+                <b>{{$order->event->organiser->tax_name}}</b>
+            </td>
+            <td colspan="2">
+                {{$orderService->getTaxAmount(true)}}
+            </td>
+        </tr>
+        @endif
+        <tr>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+            <td>
+                <b>Total</b>
+            </td>
+            <td colspan="2">
+                {{$orderService->getGrandTotal(true)}}
+            </td>
+        </tr>
+    </table>
+
+    <br><br>
+</div>
+<br><br>
+Thank you
+@stop
