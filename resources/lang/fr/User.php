2a5c0451473e0bbc2ae7abff2b0f1fc3c9541744
Bastien <bzg@gnu.org>
2018-10-07 08:57:16 +0200

    fr translate last files

--- resources/lang/fr/User.php
+++ resources/lang/fr/User.php
@@ -6,16 +6,16 @@
 return array (
   //============================== New strings to translate ==============================//
   // Defined in file C:\\wamp\\www\\attendize\\resources\\views\\Public\\LoginAndRegister\\Signup.blade.php
-  'already_have_account' => 'Déjà inscrit ? <a class="semibold" href=":url">Connectez-vous</a>',
+  'already_have_account' => 'Déjà inscrit ? <a class="semibold" href=":url">Connectez-vous</a>',
   //==================================== Translations ====================================//
   'after_welcome' => 'Pour continuer, veuillez mettre à jour votre profil avec nom et un nouveau mot de passe..',
   'change_password' => 'Changer le mot de passe',
   'confirm_new_password' => 'Confirmer le nouveau mot de passe',
-  'dont_have_account_button' => 'Pas encore inscrit ? <a class="semibold" href=":url">Créez un compte</a>',
+  'dont_have_account_button' => 'Pas encore inscrit ? <a class="semibold" href=":url">Créez un compte</a>',
   'email' => 'Email',
   'first_name' => 'Prénom',
   'forgot_password' => 'Mot de passe oublié',
-  'forgot_password?' => 'Mot de passe oublié ?',
+  'forgot_password?' => 'Mot de passe oublié ?',
   'hide_change_password' => 'Cacher le nouveau mot de passe',
   'last_name' => 'Nom',
   'login' => 'Se connecter',
@@ -31,6 +31,6 @@ return array (
   'sign_up' => 'Inscription',
   'sign_up_first_run' => 'Vous y êtes presque. Créez un compte et vous serez prêt.',
   'terms_and_conditions' => '&nbsp;&nbsp;J\'accepte <a target="_blank" href=":url"> la charte d\'utilisation </a>',
-  'welcome_to_app' => 'Bienvenue sur :app!',
+  'welcome_to_app' => 'Bienvenue sur :app !',
   'your_email' => 'Votre email',
 ;
