e895fe9dc97de3816ff024e6f8b83a6303853dc4
matometaru <matometaru@gmail.com>
2019-02-26 08:35:29 +0900

    reformat code

--- resources/lang/ja/Attendee.php
+++ resources/lang/ja/Attendee.php
@@ -1,21 +1,18 @@
 <?php
-/*************************************************************************
- Generated via "php artisan localization:missing" at 2018/04/19 17:07:35
-*************************************************************************/
 
-return array (
-  'scan_another_ticket' => '別のチケットをスキャン',
-  'scanning' => 'スキャン中',
-  'attendees' => '出席者',
-  'check_in' => 'チェックイン：：event',
-  'email' => 'Email',
-  'email_address' => 'メールアドレス',
-  'event_attendees' => 'イベント参加者',
-  'first_name' => '名',
-  'last_name' => '姓',
-  'name' => '名前',
-  'ticket' => 'チケット',
-  'reference' => '参照',
-  'search_attendees' => '参加者の検索...',
-  'send_invitation_n_ticket_to_attendee' => '出席者に招待状とチケットを送ってください。',
+return array(
+    'scan_another_ticket'                  => '別のチケットをスキャン',
+    'scanning'                             => 'スキャン中',
+    'attendees'                            => '出席者',
+    'check_in'                             => 'チェックイン：：event',
+    'email'                                => 'Email',
+    'email_address'                        => 'メールアドレス',
+    'event_attendees'                      => 'イベント参加者',
+    'first_name'                           => '名',
+    'last_name'                            => '姓',
+    'name'                                 => '名前',
+    'ticket'                               => 'チケット',
+    'reference'                            => '参照',
+    'search_attendees'                     => '参加者の検索...',
+    'send_invitation_n_ticket_to_attendee' => '出席者に招待状とチケットを送ってください。',
 );
