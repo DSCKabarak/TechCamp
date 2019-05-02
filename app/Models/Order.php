07e7866eee61eb359243ba60122551a3cf27e962
mrlexley <a.koster@live.nl>
2019-05-02 16:46:19 +0200

    Readability changes

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -176,14 +176,14 @@ class Order extends MyBaseModel
         parent::boot();
 
         static::creating(function ($order) {
-          do {
-          //generate a random string using Laravel's str_random helper
-          $token = Str::Random(5) . date('jn');
-          } //check if the token already exists and if it does, try again
-          while (Order::where('order_reference', $token)->first());
-          $order->order_reference = $token;
-          });
-
-
+            do {
+                    //generate a random string using Laravel's str_random helper
+                    $token = Str::Random(5) . date('jn');
+            } //check if the token already exists and if it does, try again
+            
+			while (Order::where('order_reference', $token)->first());
+            $order->order_reference = $token;
+        
+		});
     }
 }
