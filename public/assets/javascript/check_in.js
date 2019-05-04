74fcb2bfcb7effbd2ba3975f70b2eabf5310f47e
Max Matteo Staack <maxmatteostaack@gmail.com>
2019-05-04 19:31:53 +0200

    fix #614

--- public/assets/javascript/check_in.js
+++ public/assets/javascript/check_in.js
@@ -107,9 +107,27 @@ var checkinApp = new Vue({
             }
 
             qrcode.callback = this.QrCheckin;
-            navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-            navigator.getUserMedia({
+            // FIX SAFARI CAMERA
+            if (navigator.mediaDevices === undefined) {
+                navigator.mediaDevices = {};
+            }
+
+            if (navigator.mediaDevices.getUserMedia === undefined) {
+                navigator.mediaDevices.getUserMedia = function(constraints) {
+                    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+                    if (!getUserMedia) {
+                        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+                    }
+
+                    return new Promise(function(resolve, reject) {
+                        getUserMedia.call(navigator, constraints, resolve, reject);
+                    });
+                }
+            }
+
+            navigator.mediaDevices.getUserMedia({
                 video: {
                     facingMode: 'environment'
                 },
