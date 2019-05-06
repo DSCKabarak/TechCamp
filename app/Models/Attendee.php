db67678b0c44a178c39f5012b5d976364d1862ee
mrlexley <a.koster@live.nl>
2019-05-06 09:16:22 +0200

    Change STR length private_reference_number from 10 to 15

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -45,12 +45,12 @@ class Attendee extends MyBaseModel
         parent::boot();
 
         static::creating(function ($order) {
-			
+
             do {
                 //generate a random string using Laravel's str_random helper
-                $token = Str::Random(10);
+                $token = Str::Random(15);
             } //check if the token already exists and if it does, try again
-			
+
             while (Attendee::where('private_reference_number', $token)->first());
             $order->private_reference_number = $token;
         });
