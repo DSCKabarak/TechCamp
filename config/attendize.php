d063e8aa5a2362c344d0d3e69c947225a8f64fe1
Sebastian Schmidt <publicarray@users.noreply.github.com>
2019-05-06 21:53:01 +1000

    env -> config (analytics)
    fix php artisan config:cache

--- config/attendize.php
+++ config/attendize.php
@@ -71,5 +71,6 @@ return [
     'cdn_url_user_assets'   => '',
     'cdn_url_static_assets' => '',
 
+    'google_analytics_id' => env('GOOGLE_ANALYTICS_ID'),
     'google_maps_geocoding_key' => env('GOOGLE_MAPS_GEOCODING_KEY')
 ];
