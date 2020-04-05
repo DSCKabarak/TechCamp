4a0daa1c85201b2c5d293ed0bd712cd00fa74aac
Etienne Marais <etienne@overhq.com>
2020-04-05 15:44:45 +0200

    Use the new fluent syntax for importing csv entries

--- app/Http/Controllers/EventAttendeesController.php
+++ app/Http/Controllers/EventAttendeesController.php
@@ -2,6 +2,7 @@
 
 use App\Cancellation\OrderCancellation;
 use App\Exports\AttendeesExport;
+use App\Imports\AttendeesImport;
 use App\Jobs\GenerateTicket;
 use App\Jobs\SendAttendeeInvite;
 use App\Jobs\SendAttendeeTicket;
@@ -286,101 +287,17 @@ class EventAttendeesController extends MyBaseController
 
         }
 
-        $ticket_id = $request->get('ticket_id');
         $event = Event::findOrFail($event_id);
-        $ticket_price = 0;
-        $email_attendee = $request->get('email_ticket');
-        $num_added = 0;
+        $ticket = Ticket::scope()->find($request->get('ticket_id'));
+        $emailAttendees = $request->get('email_ticket');
         if ($request->file('attendees_list')) {
-
-            $the_file = Excel::load($request->file('attendees_list')->getRealPath(), function ($reader) {
-            })->get();
-
-            // Loop through
-            foreach ($the_file as $rows) {
-                if (!empty($rows['first_name']) && !empty($rows['last_name']) && !empty($rows['email'])) {
-                    $num_added++;
-                    $attendee_first_name = strip_tags($rows['first_name']);
-                    $attendee_last_name = strip_tags($rows['last_name']);
-                    $attendee_email = $rows['email'];
-
-                    error_log($ticket_id . ' ' . $ticket_price . ' ' . $email_attendee);
-
-
-                    /**
-                     * Create the order
-                     */
-                    $order = new Order();
-                    $order->first_name = $attendee_first_name;
-                    $order->last_name = $attendee_last_name;
-                    $order->email = $attendee_email;
-                    $order->order_status_id = config('attendize.order.complete');
-                    $order->amount = $ticket_price;
-                    $order->account_id = Auth::user()->account_id;
-                    $order->event_id = $event_id;
-
-                    // Calculating grand total including tax
-                    $orderService = new OrderService($ticket_price, 0, $event);
-                    $orderService->calculateFinalCosts();
-                    $order->taxamt = $orderService->getTaxAmount();
-
-                    if ($orderService->getGrandTotal() == 0) {
-                        $order->is_payment_received = 1;
-                    }
-
-                    $order->save();
-
-                    /**
-                     * Update qty sold
-                     */
-                    $ticket = Ticket::scope()->find($ticket_id);
-                    $ticket->increment('quantity_sold');
-                    $ticket->increment('sales_volume', $ticket_price);
-                    $ticket->event->increment('sales_volume', $ticket_price);
-
-                    /**
-                     * Insert order item
-                     */
-                    $orderItem = new OrderItem();
-                    $orderItem->title = $ticket->title;
-                    $orderItem->quantity = 1;
-                    $orderItem->order_id = $order->id;
-                    $orderItem->unit_price = $ticket_price;
-                    $orderItem->save();
-
-                    /**
-                     * Update the event stats
-                     */
-                    $event_stats = new EventStats();
-                    $event_stats->updateTicketsSoldCount($event_id, 1);
-                    $event_stats->updateTicketRevenue($ticket_id, $ticket_price);
-
-                    /**
-                     * Create the attendee
-                     */
-                    $attendee = new Attendee();
-                    $attendee->first_name = $attendee_first_name;
-                    $attendee->last_name = $attendee_last_name;
-                    $attendee->email = $attendee_email;
-                    $attendee->event_id = $event_id;
-                    $attendee->order_id = $order->id;
-                    $attendee->ticket_id = $ticket_id;
-                    $attendee->account_id = Auth::user()->account_id;
-                    $attendee->reference_index = 1;
-                    $attendee->save();
-
-                    if ($email_attendee == '1') {
-                        $this->dispatch(new SendAttendeeInvite($attendee));
-                    }
-                }
-            };
+            (new AttendeesImport($event, $ticket, (bool)$emailAttendees))->import(request()->file('attendees_list'));
         }
 
-        session()->flash('message', $num_added . ' Attendees Successfully Invited');
+        session()->flash('message', 'Attendees Successfully Invited');
 
         return response()->json([
             'status'      => 'success',
-            'id'          => $attendee->id,
             'redirectUrl' => route('showEventAttendees', [
                 'event_id' => $event_id,
             ]),
